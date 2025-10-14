53b254f39ccfcbd6f3c91f2066c4a5f050793e63
lee <183517940@qq.com>
2025-10-14 14:41:36 +0800

    feat: 修改工作组相关字段名称为员工组

--- database/migrations/tenant/2025_05_02_094340_create_user_groups_table.php
+++ database/migrations/tenant/2025_05_02_094340_create_user_groups_table.php
@@ -13,18 +13,18 @@ return new class extends Migration {
         Schema::create('user_groups', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('store_id')->comment('所属门店');
-            $table->string('name')->comment('工作组名称');
-            $table->string('description')->comment('工作组描述');
+            $table->string('name')->comment('员工组名称');
+            $table->string('description')->comment('员工组描述');
             $table->unsignedBigInteger('create_user_id')->comment('创建人员');
             $table->timestamps();
-            $table->comment('工作组表');
+            $table->comment('员工组表');
         });
         Schema::create('user_group_details', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_group_id')->comment('工作组ID');
+            $table->unsignedBigInteger('user_group_id')->comment('员工组ID');
             $table->unsignedBigInteger('user_id')->comment('用户ID');
             $table->timestamps();
-            $table->comment('工作组详情表');
+            $table->comment('员工组详情表');
         });
     }
 
