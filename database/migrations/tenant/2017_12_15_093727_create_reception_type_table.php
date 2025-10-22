185054a0fe7c2dcc26b3889564cff1e3f3aa387f
lee <183517940@qq.com>
2025-10-22 16:46:00 +0800

    feat: 添加接诊类型表的注释

--- database/migrations/tenant/2017_12_15_093727_create_reception_type_table.php
+++ database/migrations/tenant/2017_12_15_093727_create_reception_type_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('remark')->nullable()->default(null);
             $table->timestamps();
+            $table->comment('接诊类型表');
         });
     }
 
