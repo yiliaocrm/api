c335d7e1d45c8cf65476e0ee28741b761371dc07
lee <183517940@qq.com>
2025-10-30 09:58:05 +0800

    feat: 添加状态枚举和状态文本获取方法

--- app/Models/Followup.php
+++ app/Models/Followup.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
+use App\Enums\FollowupStatus;
+use Illuminate\Support\Carbon;
 use App\Observers\FollowupObserver;
+use App\Traits\QueryConditionsTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -12,9 +15,16 @@ use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 #[ObservedBy([FollowupObserver::class])]
 class Followup extends BaseModel
 {
-    use HasUuids;
+    use HasUuids, QueryConditionsTrait;
 
     protected $table = 'followup';
+
+    protected function casts(): array
+    {
+        return [
+            'status' => FollowupStatus::class,
+        ];
+    }
 
     /**
      * 顾客信息
@@ -91,5 +101,16 @@ class Followup extends BaseModel
     public function scopeToday($query)
     {
         return $query->where('followup.date', Carbon::today()->toDateString());
+    }
+
+    /**
+     * 获取类型文本
+     * @return Attribute
+     */
+    protected function statusText(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->status?->getLabel(),
+        );
     }
 }
