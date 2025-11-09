d6a98166cca2c81c0cc94d7c0c52ec812ee6d5f9
lee <183517940@qq.com>
2025-11-09 09:02:43 +0800

    feat: 为回访类型添加图标字段

--- database/seeders/Tenant/FollowupTypesTableSeeder.php
+++ database/seeders/Tenant/FollowupTypesTableSeeder.php
@@ -14,12 +14,36 @@ class FollowupTypesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        FollowupType::truncate();
-        FollowupType::create(['id' => 1, 'name' => '未上门回访']);
-        FollowupType::create(['id' => 2, 'name' => '未成交回访']);
-        FollowupType::create(['id' => 3, 'name' => '术前回访']);
-        FollowupType::create(['id' => 4, 'name' => '术后回访']);
-        FollowupType::create(['id' => 5, 'name' => '满意度回访']);
-        FollowupType::create(['id' => 6, 'name' => '活动回访']);
+        FollowupType::query()->truncate();
+        FollowupType::query()->create([
+            'id'   => 1,
+            'name' => '未上门回访',
+            'icon' => 'el-icon-warning'
+        ]);
+        FollowupType::query()->create([
+            'id'   => 2,
+            'name' => '未成交回访',
+            'icon' => 'el-icon-circle-close'
+        ]);
+        FollowupType::query()->create([
+            'id'   => 3,
+            'name' => '术前回访',
+            'icon' => 'el-icon-first-aid-kit'
+        ]);
+        FollowupType::query()->create([
+            'id'   => 4,
+            'name' => '术后回访',
+            'icon' => 'el-icon-medal'
+        ]);
+        FollowupType::query()->create([
+            'id'   => 5,
+            'name' => '满意度回访',
+            'icon' => 'el-icon-star'
+        ]);
+        FollowupType::query()->create([
+            'id'   => 6,
+            'name' => '活动回访',
+            'icon' => 'el-icon-present'
+        ]);
     }
 }
