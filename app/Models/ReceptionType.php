438df5fa19507fa30345d84b17aceb6b1a97f959
lee <183517940@qq.com>
2025-11-09 17:40:48 +0800

    refactor: 清理 ReceptionType.php 代码格式

--- app/Models/ReceptionType.php
+++ app/Models/ReceptionType.php
@@ -2,25 +2,19 @@
 
 namespace App\Models;
 
-
-
 class ReceptionType extends BaseModel
 {
-	protected $table      = 'reception_type';
-	protected $guarded    = [];
-	protected $primaryKey = 'id';
+    protected $table = 'reception_type';
 
-	public static function getInfo($id)
-	{
-		if (!$id)
-		{
-			return false;
-		}
-		static $_info = [];
-		if(!isset($_info[$id]))
-		{
-			$_info[$id] = static::find($id);
-		}
-		return $_info[$id];
-	}
+    public static function getInfo($id)
+    {
+        if (!$id) {
+            return false;
+        }
+        static $_info = [];
+        if (!isset($_info[$id])) {
+            $_info[$id] = static::find($id);
+        }
+        return $_info[$id];
+    }
 }
