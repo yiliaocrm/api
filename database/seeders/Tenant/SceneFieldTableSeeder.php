32bb3ddefa45688a622830f17734f0ce09c5cf23
lee <183517940@qq.com>
2025-11-16 16:39:09 +0800

    feat: 新增分诊接待场景化搜索配置

--- database/seeders/Tenant/SceneFieldTableSeeder.php
+++ database/seeders/Tenant/SceneFieldTableSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders\Tenant;
 
 use App\Enums\AppointmentType;
+use App\Enums\ReceptionStatus;
 use App\Enums\AppointmentStatus;
-use App\Enums\FollowupStatus;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -20,6 +20,7 @@ class SceneFieldTableSeeder extends Seeder
         $fields = array_merge(
             $this->getCustomerIndexConfig(),
             $this->getPurchaseIndexConfig(),
+            $this->getWorkbenchReceptionConfig(),
             $this->getWorkbenchFollowupConfig(),
             $this->getWorkbenchAppointmentConfig(),
             $this->getReportCustomerProductConfig(),
@@ -3608,6 +3609,279 @@ class SceneFieldTableSeeder extends Seeder
                     ['text' => '大于等于', 'value' => '>='],
                     ['text' => '小于等于', 'value' => '<='],
                     ['text' => '区间', 'value' => 'between']
+                ])
+            ],
+        ];
+    }
+
+    private function getWorkbenchReceptionConfig(): array
+    {
+        return [
+            [
+                'page'             => 'WorkbenchReception',
+                'name'             => '是否接待',
+                'table'            => 'reception',
+                'field'            => 'receptioned',
+                'field_type'       => 'tinyint',
+                'component'        => 'select',
+                'component_params' => json_encode([
+                    'props'   => [
+                        'clearable' => true
+                    ],
+                    'options' => $this->convertSettingConfigToOptions('setting.reception.receptioned')
+                ]),
+                'operators'        => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>']
+                ])
+            ],
+            [
+                'page'             => 'WorkbenchReception',
+                'name'             => '成交状态',
+                'table'            => 'reception',
+                'field'            => 'status',
+                'field_type'       => 'tinyint',
+                'component'        => 'select',
+                'component_params' => json_encode([
+                    'props'   => [
+                        'clearable' => true
+                    ],
+                    'options' => collect(ReceptionStatus::options())->map(fn($label, $value) => ['label' => $label, 'value' => $value])->values()->all()
+                ]),
+                'operators'        => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>']
+                ])
+            ],
+            [
+                'page'             => 'WorkbenchReception',
+                'name'             => '接诊类型',
+                'table'            => 'reception',
+                'field'            => 'type',
+                'field_type'       => 'tinyint',
+                'component'        => 'select',
+                'api'              => '/cache/reception-type',
+                'component_params' => json_encode([
+                    'props' => [
+                        'clearable' => true
+                    ],
+                ]),
+                'operators'        => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>']
+                ])
+            ],
+            [
+                'page'             => 'WorkbenchReception',
+                'name'             => '分诊科室',
+                'table'            => 'reception',
+                'field'            => 'department_id',
+                'field_type'       => 'int',
+                'component'        => 'select',
+                'api'              => '/cache/departments',
+                'component_params' => json_encode([
+                    'props' => [
+                        'clearable'  => true,
+                        'filterable' => true
+                    ],
+                ]),
+                'operators'        => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>']
+                ])
+            ],
+            [
+                'page'             => 'WorkbenchReception',
+                'name'             => '媒介来源',
+                'table'            => 'reception',
+                'field'            => 'medium_id',
+                'field_type'       => 'int',
+                'api'              => '/cache/mediums?cascader=1',
+                'component'        => 'cascader',
+                'operators'        => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>']
+                ]),
+                'component_params' => json_encode([
+                    'props' => [
+                        'props'      => [
+                            'label'         => 'text',
+                            'value'         => 'id',
+                            'checkStrictly' => true
+                        ],
+                        'clearable'  => true,
+                        'filterable' => true,
+                    ]
+                ]),
+                'query_config'     => json_encode([
+                    [
+                        'operator' => '=',
+                        'wheres'   => [
+                            [
+                                'type'     => 'whereRaw',
+                                'sql'      => "(cy_reception.medium_id IN (select id from cy_medium where tree LIKE CONCAT((SELECT tree FROM cy_medium WHERE id = ?), '-%') OR cy_medium.id = ?))",
+                                'bindings' => [
+                                    '{$value[-1]}',
+                                    '{$value[-1]}'
+                                ]
+                            ]
+                        ]
+                    ],
+                    [
+                        'operator' => '<>',
+                        'wheres'   => [
+                            [
+                                'type'     => 'whereRaw',
+                                'sql'      => "(cy_reception.medium_id NOT IN (select id from cy_medium where tree LIKE CONCAT((SELECT tree FROM cy_medium WHERE id = ?), '-%') OR cy_medium.id = ?))",
+                                'bindings' => [
+                                    '{$value[-1]}',
+                                    '{$value[-1]}'
+                                ]
+                            ]
+                        ]
+                    ]
+                ])
+            ],
+            [
+                'page'       => 'WorkbenchReception',
+                'name'       => '销售顾问',
+                'table'      => 'reception',
+                'field'      => 'consultant',
+                'field_type' => 'int',
+                'component'  => 'user',
+                'operators'  => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>'],
+                    ['text' => '为空', 'value' => 'is null'],
+                    ['text' => '不为空', 'value' => 'is not null']
+                ])
+            ],
+            [
+                'page'       => 'WorkbenchReception',
+                'name'       => '二开人员',
+                'table'      => 'reception',
+                'field'      => 'ek_user',
+                'field_type' => 'int',
+                'component'  => 'user',
+                'operators'  => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>'],
+                    ['text' => '为空', 'value' => 'is null'],
+                    ['text' => '不为空', 'value' => 'is not null']
+                ])
+            ],
+            [
+                'page'       => 'WorkbenchReception',
+                'name'       => '接诊医生',
+                'table'      => 'reception',
+                'field'      => 'doctor',
+                'field_type' => 'int',
+                'component'  => 'user',
+                'operators'  => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>'],
+                    ['text' => '为空', 'value' => 'is null'],
+                    ['text' => '不为空', 'value' => 'is not null']
+                ])
+            ],
+            [
+                'page'       => 'WorkbenchReception',
+                'name'       => '接待人员',
+                'table'      => 'reception',
+                'field'      => 'reception',
+                'field_type' => 'int',
+                'component'  => 'user',
+                'operators'  => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>'],
+                    ['text' => '为空', 'value' => 'is null'],
+                    ['text' => '不为空', 'value' => 'is not null']
+                ])
+            ],
+            [
+                'page'       => 'WorkbenchReception',
+                'name'       => '录单人员',
+                'table'      => 'reception',
+                'field'      => 'user_id',
+                'field_type' => 'int',
+                'component'  => 'user',
+                'operators'  => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>'],
+                    ['text' => '为空', 'value' => 'is null'],
+                    ['text' => '不为空', 'value' => 'is not null']
+                ])
+            ],
+            [
+                'page'             => 'WorkbenchReception',
+                'name'             => '咨询项目',
+                'table'            => 'reception',
+                'field'            => 'items',
+                'field_type'       => 'varchar',
+                'api'              => '/cache/items?cascader=1',
+                'component'        => 'cascader',
+                'operators'        => json_encode([
+                    ['text' => '等于', 'value' => '='],
+                    ['text' => '不等于', 'value' => '<>'],
+                    ['text' => '为空', 'value' => 'is null'],
+                    ['text' => '不为空', 'value' => 'is not null']
+                ]),
+                'component_params' => json_encode([
+                    'props' => [
+                        'props'      => [
+                            'label'         => 'name',
+                            'value'         => 'id',
+                            'checkStrictly' => true,
+                        ],
+                        'clearable'  => true,
+                        'filterable' => true
+                    ]
+                ]),
+                'query_config'     => json_encode([
+                    [
+                        'operator' => '=',
+                        'wheres'   => [
+                            [
+                                'type'     => 'whereRaw',
+                                'sql'      => "cy_reception.id IN (SELECT DISTINCT reception_id FROM cy_reception_items WHERE item_id IN (SELECT id FROM cy_item WHERE tree LIKE CONCAT((SELECT tree FROM cy_item WHERE id = ?), '-%') OR cy_item.id = ?))",
+                                'bindings' => [
+                                    '{$value[-1]}',
+                                    '{$value[-1]}'
+                                ]
+                            ]
+                        ]
+                    ],
+                    [
+                        'operator' => '<>',
+                        'wheres'   => [
+                            [
+                                'type'     => 'whereRaw',
+                                'sql'      => "cy_reception.id NOT IN (SELECT DISTINCT reception_id FROM cy_reception_items WHERE item_id IN (SELECT id FROM cy_item WHERE tree LIKE CONCAT((SELECT tree FROM cy_item WHERE id = ?), '-%') OR cy_item.id = ?))",
+                                'bindings' => [
+                                    '{$value[-1]}',
+                                    '{$value[-1]}'
+                                ]
+                            ]
+                        ]
+                    ],
+                    [
+                        'operator' => 'is null',
+                        'wheres'   => [
+                            [
+                                'type' => 'whereRaw',
+                                'sql'  => "cy_reception.id NOT IN (SELECT DISTINCT reception_id FROM cy_reception_items)",
+                            ]
+                        ]
+                    ],
+                    [
+                        'operator' => 'is not null',
+                        'wheres'   => [
+                            [
+                                'type' => 'whereRaw',
+                                'sql'  => "cy_reception.id IN (SELECT DISTINCT reception_id FROM cy_reception_items)",
+                            ]
+                        ]
+                    ]
                 ])
             ],
         ];
